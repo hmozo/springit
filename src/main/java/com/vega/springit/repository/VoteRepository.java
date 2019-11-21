76e3761d22b89ed012ea0d9a82a4c1462052ab7b
Hugo Mozo <jhmozo@hotmail.com>
2019-11-21 16:48:34 +0100

    Initial Repositories for Link&Comments

---
+++ src/main/java/com/vega/springit/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.vega.springit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<VoteRepository, Long>{
+
+}
