ab17a9155d689038c27a784c682a523809980b26
Hugo Mozo <jhmozo@hotmail.com>
2019-11-21 16:49:18 +0100

    Delete VoteRepository

--- src/main/java/com/vega/springit/repository/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vega.springit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<VoteRepository, Long>{
-
-}
